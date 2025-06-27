2eedd8e12a0b9af29e8c35f73136e716916a87bf
eleven <hutyxxx@gmail.com>
2025-06-27 13:01:11 +0800

    feat: docker

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   webpack: (config, { isServer }) => {
     // Prevent errors from specific packages
     if (!isServer) {
